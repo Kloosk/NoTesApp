5f0a59381cadee3b77c56b7b57ada2cc7a169cdf
Lukasz <lukasz.gras.97@gmail.com>
2020-11-11 23:48:54 +0100

    Change bg

--- src/components/start/video/Video.js
+++ src/components/start/video/Video.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import styled from 'styled-components'
-import notes from './file/notes.mp4'
+import bg from './file/bg.jpg'
 
 const Container = styled.div`
   position: relative;
   top: 0;
   left: 0;
-  width: 100%;
+  width: 100vw;
   height: 100vh;
+  background: url(${bg}) center/cover no-repeat;
 `;
 const Overlay = styled.div`
   background-color: #000;
   width: 100%;  
   height: 100%;
   position: absolute;
-  opacity: 0.3;
+  opacity: 0.4;
 `;
 const Video = () => {
     return (
         <Container>
             <Overlay/>
-            <video id="vid" muted loop>
-                <source type="video/mp4" src={notes}></source>
-            </video>
         </Container>
     );
 };
