ded4f01c072b7de8b4bdb73c7db0b2b2f6d66ef6
Lukasz <lukasz.gras.97@gmail.com>
2020-11-11 23:52:36 +0100

    Docs

---
+++ docs/precache-manifest.19b635d1cb97b1de04515dce07035da2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a082b4fbf24f9c61202b884a0badf09",
+    "url": "./index.html"
+  },
+  {
+    "revision": "996b87834c263504e7ad",
+    "url": "./static/js/2.b2b92abc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "./static/js/2.b2b92abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fec0e9322f9c7719ef",
+    "url": "./static/js/main.a040b222.chunk.js"
+  },
+  {
+    "revision": "bba5b8dc5cf10234e178",
+    "url": "./static/js/runtime-main.81b02d15.js"
+  },
+  {
+    "revision": "2f5417023339fb99a703e12265bf838d",
+    "url": "./static/media/bg.2f541702.jpg"
+  },
+  {
+    "revision": "0de73fa567c0cfef807eb7ed0680e77d",
+    "url": "./static/media/bgReg.0de73fa5.jpg"
+  },
+  {
+    "revision": "a53b2004b811eb64e033c85dbd283e14",
+    "url": "./static/media/load.a53b2004.gif"
+  },
+  {
+    "revision": "92051c41b9ca47db2e560409d711508d",
+    "url": "./static/media/logo.92051c41.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3e74e7e8befb7ca255bd80e55298042f.js"
+  "./precache-manifest.19b635d1cb97b1de04515dce07035da2.js"
 );
 
 self.addEventListener('message', (event) => {
