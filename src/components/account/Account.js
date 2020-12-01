c82ebc87257eb4e8ee7ad903ecb7f5d58d17311b
Lukasz <lukasz.gras.97@gmail.com>
2020-12-01 12:34:30 +0100

    Added new icon to nav

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,16 +1,54 @@
 import React from 'react';
 import styled from 'styled-components'
+import Logo from "../logo/Logo";
+import Menu from "../menu/Menu";
+import Nav from "../nav/Nav";
+import {dashClose, menuDesktopClose} from "../../redux";
+import {useDispatch} from "react-redux";
+import bg from "../../img/bgReg.jpg";
 
+const Div = styled.div`
+  position: relative;
+  overflow-x: hidden;
+  background-image: url(${bg});
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+  min-height: 100vh; 
+  padding-top: 13vh;
+  width: 100vw;
+  @media (min-width: 768px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    min-height: 100vh;
+    padding: 0;
+  }
+`;
 const Container = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   
 `;
 const Account = () => {
+    const dispatch = useDispatch();
+    const closeMenu = () => {
+        dispatch(dashClose());
+        dispatch(menuDesktopClose())
+    };
     return (
-        <Container>
+        <Div>
+            <Logo/>
+            <Menu num={4}/>
+            <Nav num={4}/>
+            <div onClick={closeMenu}>
+                <Container>
 
-        </Container>
+                </Container>
+            </div>
+        </Div>
     );
 };
 
--- src/components/publicNotes/PublicNotes.js
+++ src/components/publicNotes/PublicNotes.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import styled from 'styled-components'
+import {useDispatch} from "react-redux";
+import {dashClose, menuDesktopClose} from "../../redux";
+import Logo from "../logo/Logo";
+import Menu from "../menu/Menu";
+import Nav from "../nav/Nav";
+import bg from "../../img/bgReg.jpg";
 
+const Div = styled.div`
+  position: relative;
+  overflow-x: hidden;
+  background-image: url(${bg});
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+  min-height: 100vh; 
+  padding-top: 13vh;
+  width: 100vw;
+  @media (min-width: 768px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    min-height: 100vh;
+    padding: 0;
+  }
+`;
 const Container = styled.div`
-
+  width: 100vw;
+  min-height: 100vh;
 `;
 const PublicNotes = () => {
+    const dispatch = useDispatch();
+    const closeMenu = () => {
+        dispatch(dashClose());
+        dispatch(menuDesktopClose())
+    };
     return (
-        <Container>
-            
-        </Container>
+        <Div>
+            <Logo/>
+            <Menu num={3}/>
+            <Nav num={3}/>
+            <div onClick={closeMenu}>
+                <Container>
+
+                </Container>
+            </div>
+        </Div>
     );
 };
 
