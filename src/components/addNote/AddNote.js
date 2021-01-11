4e5290a5f803e92d4d47508bd6826dbb78b9edd0
Lukasz <lukasz.gras.97@gmail.com>
2021-01-12 00:59:17 +0100

    Rebuild html structure

--- src/components/addNote/AddNote.js
+++ src/components/addNote/AddNote.js
@@ -11,11 +11,11 @@ import {genereateOne} from "../../data/Data";
 import NoteOptions from "./noteOptions/NoteOptions";
 import bg from "../../img/bgReg.jpg"
 import Loading from "../loading/Loading";
-import LoadingAdd from "../loadingAdd/LoadingAdd";
 import NoteToolbar from "../noteToolbar/noteToolbar";
 
-const Div = styled.div`
-  position: relative;
+
+const Container = styled.div`
+  min-height: 100vh;
   overflow-x: hidden;
   background-image: ${props => props.darkmode ? "none" : `url(${bg})`};
   background-color: ${props => props.darkmode && `#231f20`};
@@ -23,40 +23,22 @@ const Div = styled.div`
   background-attachment: fixed;
   background-position: center;
   background-size: cover;
-  min-height: 100vh; 
-  padding-top: 13vh;
-  width: 100vw;
-  @media (min-width: 768px) {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    min-height: 100vh;
-    padding: 0;
-  }
-`;
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100vw;
-  @media (min-width: 768px) {
-    min-height: auto;
-    margin: 0;
-    display: grid;
-    grid-template-columns: repeat(2,1fr);
-    align-items: flex-start;
-    justify-items: center;
-    position: relative;
-  }
+  user-select: ${props => props.select ? 'none' : 'auto'};
 `;
 const Desktop = styled.div`
   display: none;
    @media (min-width: 768px) {
    display: block;
-   width: 100vw;
-   margin: 50px 0;
+   margin: 40px 0 10px 0;
 }
+`;
+const Grid = styled.div`
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    align-items: flex-start;
+    justify-items: center;
+  }
 `;
 const AddNote = () => {
     const dispatch = useDispatch();
@@ -84,20 +66,21 @@ const AddNote = () => {
         dispatch(emojiClose());
     };
     return (
-       load ? <Div darkmode={darkmode}>
-            <LoadingAdd/>
+       load ? <>
             <Logo path="/dashboard"/>
             <Menu num={1}/>
             <Nav num={1}/>
             <NoteToolbar/>
             <div onClick={closeMenu}>
-                <Desktop><StepZeroDesktop/></Desktop>
-                <Container onClick={handleClick}>
-                    <NoteEdit/>
-                    <NoteOptions/>
+                <Container onClick={handleClick} darkmode={darkmode}>
+                    <Desktop><StepZeroDesktop/></Desktop>
+                    <Grid>
+                        <NoteEdit/>
+                        <NoteOptions/>
+                    </Grid>
                 </Container>
             </div>
-        </Div> : <Loading/>
+        </> : <Loading/>
     );
 };
 
