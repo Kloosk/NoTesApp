39fafe994a8f74e9b976699a1c85b74c9527d709
Lukasz <lukasz.gras.97@gmail.com>
2021-01-11 18:16:03 +0100

    Fix loading width size

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import load from './gif/load.gif';
+import darkload from './gif/loadDark.gif'
 
 const Container = styled.div`
+  overflow: hidden;
   position: relative;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: #fff;
+  background-color: ${props => props.darkmode ? "#000" : "#fff"};
   z-index: 999;
 `;
 const Img = styled.img`
@@ -18,10 +20,17 @@ const Img = styled.img`
   transform: translate(-50%,-50%);
   width: 200px;
 `;
+const ImgDark = styled(Img)`
+  width: 50%;
+`;
 const Loading = () => {
+    const [darkmode,setDarkmode] = useState(false);
+    useEffect(() => {
+        localStorage.getItem('darkmode') !== null && setDarkmode(true);
+    },[]);
     return (
-        <Container>
-            <Img src={load} alt="Loading"/>
+        <Container darkmode={darkmode}>
+            {darkmode ? <ImgDark src={darkload} alt="Loading"/> : <Img src={load} alt="Loading"/>}
         </Container>
     );
 };
