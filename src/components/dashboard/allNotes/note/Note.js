63f3c2127ea4497049e1a7c8ba0050f98a737d29
Lukasz <lukasz.gras.97@gmail.com>
2021-01-11 17:44:03 +0100

    Fix btn and darkmode

--- src/components/dashboard/allNotes/note/Note.js
+++ src/components/dashboard/allNotes/note/Note.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components'
 import {Link} from "react-router-dom";
 import DeleteBtn from "../deleteBtn/DeleteBtn";
@@ -21,11 +21,13 @@ const Options = styled.div`
   left: 100%;
   transition: transform 1s ease,opacity 1s ease;
   opacity: 0;
+  visibility: hidden;
   transform: translateX(-100%);
   display: flex;
   flex-direction: column;
   justify-content: center;
   ${Container}:hover &{
+    visibility: visible;
     opacity: 1;
     transform: translateX(-50%);
   }
@@ -37,7 +39,7 @@ const Btn = styled.button`
   cursor: pointer;
   padding: 10px;
   position: relative;
-  background-color: #fff;
+  background-color: ${props => props.darkmode ? "#231F20" : "#fff"};
 `;
 const Svg = styled.svg`
   color: #F9B613;
@@ -76,12 +78,16 @@ const Title = styled(Textarea)`
   overflow-x: hidden;
 `;
 const Note = ({title,text,titleColor,titleBg,textColor,textBg,border,borderSize,font,textSize,titleSize,num,titleTransform,textTransform,alignDesc}) => {
+    const [darkmode,setDarkmode] = useState(false);
+    useEffect(() => {
+        localStorage.getItem('darkmode') !== null && setDarkmode(true);
+    },[]);
     return (
         <Container border={border} borderSize={borderSize}>
             <Options>
-                <DeleteBtn id={num}/>
+                <DeleteBtn id={num} darkmode={darkmode}/>
                 <Link to={`/edit/${num}`}>
-                    <Btn>
+                    <Btn darkmode={darkmode}>
                         <Svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pencil-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"></path></Svg>
                     </Btn>
                 </Link>
--- src/components/edit/submitEdit/SubmitEdit.js
+++ src/components/edit/submitEdit/SubmitEdit.js
@@ -10,7 +10,7 @@ const Container = styled.button`
   font-size: 1.2rem;
   margin: 3px 0;
   padding: 7px 14px;
-  background: #E8EAE3;
+  background: transparent;
   border: 2px solid #F9B613;
   color: #F9B613;
   position: relative;
@@ -20,22 +20,8 @@ const Container = styled.button`
   @media (min-width: 768px) {
     width: 205px;
  }
-  &::before{
-    z-index: -1;
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transition: transform 0.5s ease, opacity 0.5s ease;
-    transform: translate(-50%,-50%) scaleX(0) skewX(4deg);
-    opacity: 0;
-    height: 100%;
-    width: 10%;
-    background-color: #0798DA;
-  }
-  :hover::before{
-    opacity: 1;
-    transform: translate(-50%,-50%) scaleX(10) skewX(4deg);
+  :hover{
+    transform: translateY(1px);
   }
 `;
 const SubmitEdit = ({edit}) => {
