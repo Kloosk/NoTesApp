55bf88845f0448fc9e9d5683fd2444ed1d311faa
Lukasz <lukasz.gras.97@gmail.com>
2021-01-12 01:40:46 +0100

    Change description and fix structure

--- src/components/touse/Touse.js
+++ src/components/touse/Touse.js
@@ -9,45 +9,43 @@ import {dashClose, menuDesktopClose} from "../../redux";
 import {useDispatch} from "react-redux";
 import bg from "../../img/bgReg.jpg"
 
-const Bg = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
+const Container = styled.div`
   min-height: 100vh;
+  overflow-x: hidden;
   background-image: ${props => props.darkmode ? "none" : `url(${bg})`};
   background-color: ${props => props.darkmode && `#231f20`};
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-position: center;
   background-size: cover;
+  user-select: ${props => props.select ? 'none' : 'auto'};
+  padding-top: 70px;
+  @media (min-width: 768px) {
+      padding-top: 50px;
+   }
+  }
 `;
-
-const Container = styled.div`
-  width: 100vw;
+const Grid = styled.div`
+  margin: 30px 0;
   display: grid;
   grid-template-columns: 1fr;
   grid-gap: 15px;
-  margin: 10vh 0;
   justify-items: center;
   @media (min-width: 768px) {
     grid-template-columns: repeat(3,1fr);
     grid-gap: 50px;
-    padding: 0 50px;
+    margin: 50px;
   }
 `;
 const H1 = styled.h1`
   width: 100vw;
   text-align: center;
-  font-size: 2.2rem;
+  font-size: 2rem;
   color: #F9B613;
-  margin: 10vh 0 0 0;
   @media (min-width: 768px) {
-    font-size: 3rem;
-    margin: 5vh 0 0 0;
+    font-size: 2.5rem;
+    margin: 0;
   }
-`;
-const Span = styled.span`
-  color: #0798DA;
 `;
 const Touse = () => {
    const dispatch = useDispatch();
@@ -61,15 +59,17 @@ const Touse = () => {
         dispatch(menuDesktopClose())
     };
     return (
-        <Bg darkmode={darkmode}>
+        <>
             <Logo/>
             <Menu/>
             <Nav/>
-            <H1><Span>C</Span>hoose</H1>
-            <Container onClick={closeMenu}>
-                {notesExample.map((el,i) => <Note key={i} titlecolor={el.titleColor} titlebg={el.titleBg} textcolor={el.textColor} textbg={el.textBg} border={el.border} font={el.font}/>)}
+            <Container onClick={closeMenu} darkmode={darkmode}>
+                <H1>Select by click</H1>
+                <Grid>
+                    {notesExample.map((el,i) => <Note key={i} titlecolor={el.titleColor} titlebg={el.titleBg} textcolor={el.textColor} textbg={el.textBg} border={el.border} font={el.font}/>)}
+                </Grid>
             </Container>
-        </Bg>
+        </>
     );
 };
 
